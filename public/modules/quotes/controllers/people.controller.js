348071edf868cf75ec88c265d1851e7522962285
Ravikumar <ravikumargh13@gmail.com>
2016-05-08 23:13:00 +0530

    add driver and policy holder

--- public/modules/quotes/controllers/people.controller.js
+++ public/modules/quotes/controllers/people.controller.js
@@ -7,16 +7,51 @@ angular.module('quote').controller('PeopleController', ['$scope', '$location', '
 		$scope.authentication = Authentication;
 		var Driver={
 			name:'',
-			age:''
+			age:'',
+			color:''
+		}
+		$scope.colors = [
+	      {name:'white',isAdded:false,isPrimary: true},
+	      {name:'black',isAdded:false},
+	      {name:'red',isAdded:false},
+	      {name:'blue',isAdded:false},
+	      {name:'yellow',isAdded:false}
+	    ];
+	    $scope.colorsTemp=angular.copy($scope.colors);
+		
+		$scope.update=function(driver){
+			debugger;
+			driver.color=this.selectedColor.name;
 		}
 		$scope.people=$scope.$parent.quote.people;
+		var diverIndex=0;
 		$scope.addDriver=function(){
 			var driver=angular.copy(Driver);
-			driver.name="driver 1";
-			driver.age="age 1";
+			driver.name="driver "+diverIndex;
+			driver.age="age "+diverIndex;
+			diverIndex++;
 			$scope.$parent.quote.people.drivers.push(driver);
+			
+			if(diverIndex>1){
+				for (var i = $scope.colorsTemp.length - 1; i >= 0; i--) {
+				if($scope.colorsTemp[i].name===this.selectedColor.name){
+					$scope.colors.splice(i+1, 1);
+				}
+			};
+				//var index = $scope.colors.indexOf(this.selectedColor.name);
+			  	//$scope.colors.splice(index+1, 1);
+			  	//$scope.colors[index+1].isAdded=true; 
+			}
+
+			this.selectedColor=$scope.colors[0];
 		}
 		$scope.removeDriver=function(driver,index){
+			for (var i = $scope.colorsTemp.length - 1; i >= 0; i--) {
+				if($scope.colorsTemp[i].name===this.selectedColor.name){
+					$scope.colors.push($scope.colorsTemp[i]);
+				}
+			};
+			 
 			$scope.people.drivers.splice(index,1);
 		}
 
